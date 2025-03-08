9167c82ebcf6b0b63ba071dfb4218482bab8fe7b
Osmar Montesinos <osmarmontes50@gmail.com>
2025-03-07 20:57:43 -0500

    avance

--- Semana2/Día3/03-dom.js
+++ Semana2/Día3/03-dom.js
@@ -29,8 +29,38 @@ parrafos.forEach((p) => {
 const inputName = document.querySelector("#input-name");
 const buttonName = document.getElementById("btn");
 
+//agregar atributos
+inputName.setAttribute("placeholder", "Ej. Juan Perez");
+
 //elemento.addEventListener("nombreevento", funcionAEjecutar)
 buttonName.addEventListener("click", () => {
     alert(`El valor del input es ${inputName.value}`);
 })
+//`-----------------------------
+const footer = document.querySelector("footer");
 
+footer.innerHTML = `
+    <h3>Información</h3>
+    <ul>
+        <li>Correo: info@dominio.com</li>
+        <li>Whatsapp: +99634523453245</li>
+    </ul>
+    <button id="btn-info">Más información</button>
+`;
+
+const buttonInfo = document.getElementById("btn-info");
+
+buttonInfo.style.backgroundColor = "skyblue";
+
+//crear desde JS un nuevo elemento (elemento HTML)
+const img = document.createElement("img");
+img.setAttribute("src", "https://www.infobae.com/resizer/v2/6VHUWMMTEJF2RKLO3NRADENBJI.jpg?auth=8de7b85b5f92ab2dfe221ba63f7f9634094fa8114ad1952408af3306f81043b2&smart=true&width=350&height=467&quality=85");
+img.alt = "imagen referente día de la mujer";
+
+console.log(img);
+
+//preprend para agregar al inicio
+// footer.prepend(img);
+
+//appendChild para ahrehar como último elemento
+footer.appendChild(img);
