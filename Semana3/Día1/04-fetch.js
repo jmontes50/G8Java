b830cc097e9d22b633adb688389c9f28be7ea5ef
Osmar Montesinos <osmarmontes50@gmail.com>
2025-03-10 21:58:16 -0500

    avance

---
+++ Semana3/Día1/04-fetch.js
@@ -0,0 +1,39 @@
+/*
+//GET
+fetch('https://reqres.in/api/users?page=2')
+.then((response) => {
+    console.log(response);
+    return response.json();
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => {
+    console.log(error);
+})
+*/
+const user = {
+    name:"Jorge",
+    job:"Developer"
+}
+
+const config = {
+    method:"POST",
+    //
+    body: JSON.stringify(user),
+    headers: {
+        "Content-Type": "application/json"
+    }
+}
+
+fetch("https://reqres.in/api/users", config)
+.then((res) => {
+    console.log(res);
+    return res.json();
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((err) => {
+    console.log(err);
+})
