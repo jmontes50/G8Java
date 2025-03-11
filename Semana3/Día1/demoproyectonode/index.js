1f37da811d15f374623e6ac09dba0872b6eed801
Osmar Montesinos <osmarmontes50@gmail.com>
2025-03-10 22:56:58 -0500

    avance

--- Semana3/Día1/demoproyectonode/index.js
+++ Semana3/Día1/demoproyectonode/index.js
@@ -4,7 +4,7 @@
 const axios = require('axios');
 //En el caso de las librerias que tengamos en los node_modules vamos a poder referenciarlo directamente por su nombre
 
-console.log("EJECUTANDO...");
+console.log("PREVIO A HACER LA PETICIÓN...");
 
 axios("https://reqres.in/api/users?page=2")
 .then((rpta) => console.log(rpta));
