5df70ee68b79256751144578a38e7343dc9d7225
Osmar Montesinos <osmarmontes50@gmail.com>
2025-03-10 21:06:06 -0500

    avance

--- Semana3/Día1/03-asyncawait.js
+++ Semana3/Día1/03-asyncawait.js
@@ -10,15 +10,35 @@
 // }
 
 async function obtenerDatos() {
+    // setTimeout(() => {
+    //     return "datos obtenidos"
+    // }, 2000)
     return "datos obtenidos"; //va a ser el equivalente de un resolve
     // throw "Ocurrió un error"; //va a ser el equivalente de un reject
 }
 
+const validarDatos = async () => {
+    return "Datos validados correctamente";
+}
 
-obtenerDatos()
-.then((res) => {
-    console.log(res);
-})
-.catch((err) => {
-    console.log(err);
-});
+// obtenerDatos()
+// .then((res) => {
+//     console.log(res);
+// })
+// .catch((err) => {
+//     console.log(err);
+// });
+
+const ejecutarOrdenes = async () => {
+    try {
+        //código asíncrono que deseo ejecutar
+        const res = await obtenerDatos();
+        //encadenamiento de promesas
+        const dataValidada = await validarDatos();
+        console.log(dataValidada);
+    } catch (error) {
+        //manejo de errores
+        console.log(error);
+    }
+}
+ejecutarOrdenes();
