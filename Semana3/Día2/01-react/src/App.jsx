7adb9e11d2d3023acb342f8d32bd650dc59c9127
Osmar Montesinos <osmarmontes50@gmail.com>
2025-03-12 20:36:18 -0500

    avance

--- Semana3/Día2/01-react/src/App.jsx
+++ Semana3/Día2/01-react/src/App.jsx
@@ -1,10 +1,33 @@
+//Podemos importar css o cualquier otro archivo para ser utilizado, pero en el caso de importar CSS este será utilizado en cualquier parte del proyecto
+import "./App.css";
+//La función del componente tiene que estar nombrada iniciando con Mayúsculas
 const App = () => {
+  const titulo = "Hello React!!";
 
+  //Todo componente tiene que retornar jsx (su extensión tiene que ser .jsx)
+  //y lo que retornemos es lo que se va a renderizar en la app
   return (
-    <header>
-      <h1>Hola React!</h1>
-    </header>
+    //SOLAMENTE se puede retornar 01 elemento en el JSX
+    //<></> Fragment
+    <>
+      <header className="heading">
+        {/* En JSX Si necesitamos referenciar o usar JS, basta con abrir {} y escribir dentro nuestro código de js */}
+        <h1>{titulo}</h1>
+      </header>
+      <main>
+        <h2>Título</h2>
+        <section>
+          {/* Para indicar algunos atributos con nombres iguales a los de html y js, se utilizando nombres diferentes */}
+          <label htmlFor="correo">Correo:</label>
+          <input type="email" id="correo" />
+        </section>
+        <section>
+          <p>Lorem ipsum dolor sit. {100 + 900}</p>
+        </section>
+      </main>
+    </>
   )
 }
 
+//Esto tiene que ser exportado
 export default App;
