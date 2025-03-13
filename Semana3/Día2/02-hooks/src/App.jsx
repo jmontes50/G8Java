20ef080c092ed9b5edd80ddfe169f43b74b4d777
Osmar Montesinos <osmarmontes50@gmail.com>
2025-03-12 22:24:27 -0500

    avance

---
+++ Semana3/Día2/02-hooks/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+const App = () => {
+  // const [nombreEstado, funcionEstado] = useState(valorInicial);
+  //cuando cambie de valor el estado, esto se verá reflejado automáticamente en la interfaz
+  let [contador, setContador] = useState(0);
+
+  const incrementar = () => {
+    setContador(contador + 1)
+  }
+
+  const disminuir = () => {
+    setContador(contador - 1)
+  }
+
+  const cambiar = (valor) => {
+    setContador(valor)
+  }
+
+  return (
+    <>
+    <h1>App</h1>
+    <h3>Contador: {contador}</h3>
+    <button onClick={incrementar}>
+      Incrementar
+    </button>
+    <button onClick={disminuir}>
+      Disminuir
+    </button>
+    <button onClick={() => {cambiar(1000)}}>
+      cambiar
+    </button>
+    </>
+  )
+}
+
+export default App
