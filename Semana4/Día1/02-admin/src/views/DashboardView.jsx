1c0562346356f62814691535e4e44dd741336204
Osmar Montesinos <osmarmontes50@gmail.com>
2025-03-17 23:00:21 -0500

    avance

--- Semana4/Día1/02-admin/src/views/DashboardView.jsx
+++ Semana4/Día1/02-admin/src/views/DashboardView.jsx
@@ -8,7 +8,8 @@ const DashboardView = () => {
     const getProducts = async () => {
       try {
         const productsObtained = await requestProducts();
-        console.log(productsObtained);
+        // console.log(productsObtained);
+        setProducts(productsObtained);
       } catch (error) {
         console.log(error)
       }
@@ -16,7 +17,25 @@ const DashboardView = () => {
     getProducts();
   });
 
-  return <div>DashboardView</div>;
+  return <div>
+    <h2>Dashboard View</h2>
+    <table className="border-collapse border border-gray-400">
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Precio</th>
+        </tr>
+      </thead>
+      <tbody>
+        {products.map((prod) => (
+          <tr key={prod.id}>
+            <td>{prod.nombre}</td>
+            <td>{prod.precio}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>;
 };
 
 export default DashboardView;
