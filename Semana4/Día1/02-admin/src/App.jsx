c373276a682101eeeff461f20b7c34748e3855cd
Osmar Montesinos <osmarmontes50@gmail.com>
2025-03-17 22:35:27 -0500

    avance

--- Semana4/Día1/02-admin/src/App.jsx
+++ Semana4/Día1/02-admin/src/App.jsx
@@ -1,7 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DashboardView from "./views/DashboardView";
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <div>App</div>
+      <Routes>
+        <Route path='/' element={<DashboardView />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
