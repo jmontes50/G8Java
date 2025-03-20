50120252e63567130c2f6381060149b5bb594c7d
Osmar Montesinos <osmarmontes50@gmail.com>
2025-03-19 20:13:44 -0500

    avance

--- Semana4/Día1/02-admin/src/components/TableData.jsx
+++ Semana4/Día1/02-admin/src/components/TableData.jsx
@@ -2,11 +2,11 @@ const TableData = (props) => {
   const { data, headers } = props;
   console.log(data);
   return (
-    <table className="border-collapse border border-r-gray-300 w-full">
+    <table className="border-collapse border border-gray-300 w-full">
       <thead>
         <tr>
           {headers.map((item, i) => (
-            <th key={i} className="border border-r-gray-300">
+            <th key={i} className="border border-gray-300 p-1">
               {item.label}
             </th>
           ))}
@@ -17,7 +17,7 @@ const TableData = (props) => {
           <tr key={item.id}>
             {headers.map((head, i) => (
               // brackets notation obj.prop obj['prop']
-              <td key={i}>
+              <td key={i} className="border border-gray-300 p-1">
                 {/* preguntamos la propiedad format existe, V, aplica la transformación, si no, directamente coloca el dato */}
                 { head.format ? head.format(item[head.name]) : item[head.name] }
               </td>
