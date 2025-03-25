2e9c63968688124e7f3d58dca2850e771719922f
Osmar Montesinos <osmarmontes50@gmail.com>
2025-03-24 22:45:36 -0500

    avance

--- Semana4/Día1/02-admin/src/views/EditProductView.jsx
+++ Semana4/Día1/02-admin/src/views/EditProductView.jsx
@@ -1,16 +1,36 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { requestProductById, requestUpdateProduct } from "../services/productService";
+import Input from "../components/Input";
+import { uploadFile } from "../services/supabaseService";
+import { requestCategories } from "../services/categoryService";
+import Swal from "sweetalert2";
 
 const EditProductView = () => {
-  const [product, setProduct] = useState({})
+  const [product, setProduct] = useState(null)
   const { id } = useParams();
+
+  const handleInput = (event) => {
+    setProduct({ ...product, [event.target.name]: event.target.value });
+  };
+
+  const inputs = [
+    { name: "nombre", label: "Nombre producto", type: "text" },
+    { name: "descripcion", label: "Descripción producto", type: "text" },
+    { name: "precio", label: "Precio producto", type: "number" },
+    { name: "precio_oferta", label: "Precio de oferta", type: "number" },
+    { name: "detalles", label: "Detalles", type: "text" },
+    { name: "cantidad", label: "Stock", type: "number" },
+    { name: "estrellas", label: "Estrellas", type: "number" },
+  ];
 
   useEffect(() => {
     const getProductById = async () => {
+      console.log("pre")
       try {
         const prod = await requestProductById(id);
-        console.log(prod);
+        console.log("after requestById", prod)
+        setProduct(prod);
       } catch (error) {
         console.log(error)
       }
@@ -19,7 +39,23 @@ const EditProductView = () => {
   },[])
 
   return (
-    <div>EditProductView</div>
+    <form>
+      {!product ? <>
+        Espere por favor
+      </> : <>
+        {inputs.map((item, i) => (
+          <Input
+            key={i}
+            value={product}
+            nameProp={item.name}
+            label={item.label}
+            type={item.type}
+            handleInput={handleInput}
+          />
+        ))}
+      </>}
+      <h1>Hola</h1>
+    </form>
   )
 }
 
