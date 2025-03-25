1a4b90c68497bdcea715c71b7c04d28be5d2cf57
Osmar Montesinos <osmarmontes50@gmail.com>
2025-03-24 22:24:23 -0500

    avance

--- Semana4/Día1/02-admin/src/views/EditProductView.jsx
+++ Semana4/Día1/02-admin/src/views/EditProductView.jsx
@@ -1,9 +1,23 @@
-import { useParams } from "react-router-dom"
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { requestProductById, requestUpdateProduct } from "../services/productService";
 
 const EditProductView = () => {
+  const [product, setProduct] = useState({})
   const { id } = useParams();
-  console.log(id);
-  
+
+  useEffect(() => {
+    const getProductById = async () => {
+      try {
+        const prod = await requestProductById(id);
+        console.log(prod);
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getProductById();
+  },[])
+
   return (
     <div>EditProductView</div>
   )
