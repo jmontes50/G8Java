658ac99b695b78e8a19b9e351b2b4a41e1b57843
Osmar Montesinos <osmarmontes50@gmail.com>
2025-03-31 20:28:35 -0500

    avance

--- Semana5/Día3/03-ecommerce/src/App.jsx
+++ Semana5/Día3/03-ecommerce/src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./modules/ui/components/Navbar";
 import ProductPage from "./modules/Products";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<ProductPage />} />
       </Routes>
---
+++ Semana5/Día3/03-ecommerce/src/modules/ui/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <a className="btn btn-ghost text-xl">Java's Store</a>
+    </div>
+  );
+};
+
+export default Navbar;
