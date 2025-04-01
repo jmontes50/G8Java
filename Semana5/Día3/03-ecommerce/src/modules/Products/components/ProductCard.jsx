5334725e5e86f3f8cc625298ef48c6207f08791b
Osmar Montesinos <osmarmontes50@gmail.com>
2025-03-31 19:57:28 -0500

    avance

--- Semana5/Día3/03-ecommerce/src/modules/Products/components/ProductCard.jsx
+++ Semana5/Día3/03-ecommerce/src/modules/Products/components/ProductCard.jsx
@@ -1,20 +1,20 @@
 const ProductCard = ({ product }) => {
+  const { id, nombre, imagen, descripcion } = product;
+
   return (
     <div className="card bg-base-100 w-full shadow-sm">
-      <figure>
-        <img src="" alt="" />
-      </figure>
+      <picture className="h-[420px] w-full">
+        <img src={imagen} alt={`imagen de ${nombre}`} className="w-full h-full object-cover"/>
+      </picture>
       <div className="card-body">
-        <h2 className="card-title">Product</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, tempora.</p>
+        <h2 className="card-title">{nombre}</h2>
+        <p>{descripcion}</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">
-            Ver
-          </button>
+          <button className="btn btn-primary">Ver</button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
--- Semana5/Día3/03-ecommerce/src/modules/Products/index.jsx
+++ Semana5/Día3/03-ecommerce/src/modules/Products/index.jsx
@@ -1,12 +1,23 @@
-import useGetAxios from "../../hooks/useGetAxios"
+import useGetAxios from "../../hooks/useGetAxios";
+import ProductCard from "./components/ProductCard";
 
 const ProductPage = () => {
-  const { data, loading } = useGetAxios('https://json-server-vercel-eosin-tau.vercel.app/products');
-  console.table({ data, loading });
+  const { data, loading } = useGetAxios(
+    "https://json-server-vercel-eosin-tau.vercel.app/products"
+  );
 
   return (
-    <div>index</div>
-  )
-}
+    <div>
+      <h1 className="text-3xl my-6">Catálogo</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        {data
+          ? data.map((product) => (
+              <ProductCard product={product} key={product.id} />
+            ))
+          : null}
+      </div>
+    </div>
+  );
+};
 
-export default ProductPage
+export default ProductPage;
