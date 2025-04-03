b0d07f8447e77ab2ec20228b65ef99f1b0c39175
Osmar Montesinos <osmarmontes50@gmail.com>
2025-04-02 22:35:18 -0500

    avance

--- Semana5/Día3/03-ecommerce/src/modules/Cart/CartPage.jsx
+++ Semana5/Día3/03-ecommerce/src/modules/Cart/CartPage.jsx
@@ -3,6 +3,8 @@ import useCartStore from "../../stores/useCartStore";
 const CartPage = () => {
   const { cart } = useCartStore();
 
+  const total = cart.reduce((acum, item) => acum + (item.precio * item.qtyCart), 0);
+
   return (
     <div className="px-4 py-10 mx-auto container max-w-7xl">
       <h2 className="text-3xl font-semibold mb-5">Checkout</h2>
@@ -30,15 +32,19 @@ const CartPage = () => {
                 <tr key={prod.id} className="border-b-2">
                   <td className="px-6 py-4">{prod.nombre}</td>
                   <td className="px-6 py-4">S/ {prod.precio.toFixed(2)}</td>
-                  <td className="px-6 py-4">{prod.cantidad}</td>
-                  <td className="px-6 py-4">S/ {(prod.cantidad * prod.precio).toFixed(2)}</td>
+                  <td className="px-6 py-4">{prod.qtyCart}</td>
+                  <td className="px-6 py-4">S/ {(prod.qtyCart * prod.precio).toFixed(2)}</td>
                 </tr>
               )) : null}
             </tbody>
           </table>
         </div>
-        <div className="col-span-1">
-
+        {/* descuento */}
+        <div className="col-span-1 border-2">
+          <div className="flex justify-between p-6 font-bold border-b-2">
+            <span>Subtotal</span>
+            <span>S/ { total.toFixed(2) }</span>
+          </div>
         </div>
       </div>
     </div>
