00f66453bdf5baa52b6697555efee9e667b47d18
Osmar Montesinos <osmarmontes50@gmail.com>
2025-04-02 22:20:56 -0500

    avance

--- Semana5/Día3/03-ecommerce/src/modules/Cart/CartPage.jsx
+++ Semana5/Día3/03-ecommerce/src/modules/Cart/CartPage.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import useCartStore from "../../stores/useCartStore";
 
 const CartPage = () => {
-  return (
-    <div>CartPage</div>
-  )
-}
+  const { cart } = useCartStore();
 
-export default CartPage
+  return (
+    <div className="px-4 py-10 mx-auto container max-w-7xl">
+      <h2 className="text-3xl font-semibold mb-5">Checkout</h2>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
+        <div className="col-span-2">
+          <table className="w-full">
+            <thead className="text-xs uppercase">
+              <tr>
+                <th className="px-6 py-3 text-left">
+                  Producto
+                </th>
+                <th className="px-6 py-3 text-left">
+                  Precio Unit.
+                </th>
+                <th className="px-6 py-3 text-left">
+                  Cantidad
+                </th>
+                <th className="px-6 py-3 text-left">
+                  Sub-total
+                </th>
+              </tr>
+            </thead>
+            <tbody className="text-sm">
+              { cart ? cart.map((prod) => (
+                <tr key={prod.id} className="border-b-2">
+                  <td className="px-6 py-4">{prod.nombre}</td>
+                  <td className="px-6 py-4">{prod.precio}</td>
+                  <td className="px-6 py-4">{prod.cantidad}</td>
+                  <td className="px-6 py-4">{prod.cantidad * prod.precio}</td>
+                </tr>
+              )) : null}
+            </tbody>
+          </table>
+        </div>
+        <div className="col-span-1">
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartPage;
