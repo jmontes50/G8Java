89ac79898aee700de6f42ba147e1bc474fd789c8
Osmar Montesinos <osmarmontes50@gmail.com>
2025-04-07 22:30:58 -0500

    avance

--- Semana5/Día3/03-ecommerce/src/modules/ui/components/Map.jsx
+++ Semana5/Día3/03-ecommerce/src/modules/ui/components/Map.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   MapContainer,
   Marker,
@@ -9,6 +9,7 @@ import {
 } from "react-leaflet";
 
 const Map = () => {
+  const [mapCoords, setMapCoords] = useState([-12.06, -77.02]);
   const [positionMarker, setPositionMarker] = useState(null);
 
   const MapActions = () => {
@@ -20,14 +21,28 @@ const Map = () => {
       }
     })
 
+    if(mapCoords){
+      map.flyTo(mapCoords);
+    }
+
     return <>
       {positionMarker ? <Marker position={positionMarker} /> : null}
     </>
   }
 
+  useEffect(() => {
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition((position) => {
+        // console.log(position);
+        const { latitude, longitude } = position.coords;
+        setMapCoords([latitude, longitude]);
+      })
+    }
+  },[])
+
   return (
     <div className="w-full border-2 rounded h-[450px] my-4">
-      <MapContainer center={[-12.06, -77.02]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer center={mapCoords} zoom={13} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
