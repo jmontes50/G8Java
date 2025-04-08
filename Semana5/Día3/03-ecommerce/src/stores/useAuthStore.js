8e068e2f3a57d9d0515517ed2ef428e7cd762e88
Osmar Montesinos <osmarmontes50@gmail.com>
2025-04-07 22:58:03 -0500

    avance

---
+++ Semana5/Día3/03-ecommerce/src/stores/useAuthStore.js
@@ -0,0 +1,44 @@
+import { create } from "zustand";
+import { devtools } from "zustand/middleware";
+import { saveStorage, getStorage } from "../utils/localStorage";
+import axios from "axios";
+
+const useAuthStore = create(
+  devtools((set) => ({
+    user: null,
+    token: null, //jwt
+    isLogged: false,
+
+    login: async (email, password) => {
+      try {
+        const user = {
+          email,
+          password,
+        };
+        const response = await axios.post(
+          "https://json-server-vercel-eosin-tau.vercel.app/login",
+          user
+        );
+        if (response.status === 200) {
+          //la API esta devolviendo al loguearse:
+          /**
+         * {
+              "accessToken": "",
+              "user": {
+                  "email": "",
+                  "name": ""
+              }
+          }
+         */
+          const { accessToken, user } = response.data;
+          saveStorage("token", accessToken);
+          set({ user, token: accessToken, isLogged: true });
+        }
+      } catch (error) {}
+    },
+    logout: () => {
+      localStorage.removeItem("token");
+      set({ token: null, isLogged: false, user: null });
+    },
+  }))
+);
