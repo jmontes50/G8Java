d3a781266926e00b25d38635704b31640ce33616
Osmar Montesinos <osmarmontes50@gmail.com>
2025-04-09 22:10:03 -0500

    avance

--- Semana5/Día3/03-ecommerce/src/App.jsx
+++ Semana5/Día3/03-ecommerce/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import Navbar from "./modules/ui/components/Navbar";
+import ProtectedRoute from "./modules/Auth/components/ProtectedRoute";
 import ProductPage from "./modules/Products";
 import ProductDetailPage from "./modules/Products/ProductDetailPage";
 import CartPage from "./modules/Cart/CartPage";
@@ -14,13 +15,20 @@ const App = () => {
       <Routes>
         <Route path="/" element={<ProductPage />} />
         <Route path="/productdetail/:id" element={<ProductDetailPage />} />
-        <Route path="/cart" element={<CartPage />} />
+        <Route
+          path="/cart"
+          element={
+            <ProtectedRoute>
+              <CartPage />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/article" element={<Article />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
       <ToastContainer />
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
